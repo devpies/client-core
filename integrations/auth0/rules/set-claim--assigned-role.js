1b2336dc736728025c605e653ece19d0b6205005
Ivor Scott Cummings <ivor@devpie.io>
2022-05-09 00:57:38 +0200

    Update set-claim--assigned-role.js

--- integrations/auth0/rules/set-claim--assigned-role.js
+++ integrations/auth0/rules/set-claim--assigned-role.js
@@ -1,5 +1,5 @@
 function SetAssignedRoles(user, context, callback) {
-    const namespace = 'https://client.devpie.io/claims/roles';
+    const namespace = 'https://client-domain.com/claims/roles';
     const assignedRoles = (context.authorization || {}).roles;
 
     user.user_metadata = user.user_metadata || {};
